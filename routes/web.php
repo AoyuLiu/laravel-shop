e417744e70eda03f7e3a7cbebc011c38cbc3d0fe
AoyuLiu <liuaoyu2012@yeah.net>
2018-08-18 11:03:21 +0000

    用户界面 -商品列表

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 Auth::routes();
 
@@ -27,6 +27,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     	Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     	Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+      
     });
 });
 
