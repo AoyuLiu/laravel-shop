72576d160157da80b5e8a985f014afde22d0ba78
AoyuLiu <liuaoyu2012@yeah.net>
2018-08-26 09:30:52 +0000

    管理后台 -新增、修改、删除优惠卷

--- app/Admin/Controllers/CouponCodesController.php
+++ app/Admin/Controllers/CouponCodesController.php
@@ -24,55 +24,53 @@ class CouponCodesController extends Controller
         });
     }
 
-    /**
-     * Show interface.
-     *
-     * @param mixed   $id
-     * @param Content $content
-     * @return Content
-     */
-    public function show($id, Content $content)
+     public function edit($id)
     {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header('编辑优惠券');
+            $content->body($this->form()->edit($id));
+        });
     }
 
-    /**
-     * Edit interface.
-     *
-     * @param mixed   $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
+    public function create()
     {
-        return $content
-            ->header('Edit')
-            ->description('description')
-            ->body($this->form()->edit($id));
+        return Admin::content(function (Content $content) {
+            $content->header('新增优惠券');
+            $content->body($this->form());
+        });
     }
 
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
+    public function form()
     {
-        return $content
-            ->header('Create')
-            ->description('description')
-            ->body($this->form());
+        return Admin::form(CouponCode::class, function (Form $form) {
+            $form->display('id', 'ID');
+            $form->text('name', '名称')->rules('required');
+            $form->text('code', '优惠码')->rules('nullable|unique:coupon_codes');
+            $form->radio('type', '类型')->options(CouponCode::$typeMap)->rules('required');
+            $form->text('value', '折扣')->rules(function ($form) {
+                if ($form->type === CouponCode::TYPE_PERCENT) {
+                    // 如果选择了百分比折扣类型，那么折扣范围只能是 1 ~ 99
+                    return 'required|numeric|between:1,99';
+                } else {
+                    // 否则只要大等于 0.01 即可
+                    return 'required|numeric|min:0.01';
+                }
+            });
+            $form->text('total', '总量')->rules('required|numeric|min:0');
+            $form->text('min_amount', '最低金额')->rules('required|numeric|min:0');
+            $form->datetime('not_before', '开始时间');
+            $form->datetime('not_after', '结束时间');
+            $form->radio('enabled', '启用')->options(['1' => '是', '0' => '否']);
+
+            $form->saving(function (Form $form) {
+                if (!$form->code) {
+                    $form->code = CouponCode::findAvailableCode();
+                }
+            });
+        });
     }
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
+    
     protected function grid()
     {
         return Admin::grid(CouponCode::class, function (Grid $grid) {
@@ -91,56 +89,11 @@ class CouponCodesController extends Controller
             });
             $grid->created_at('创建时间');
 
+            $grid->actions(function ($actions) {
+                $actions->disableView();
+            });
+
         });
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed   $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(CouponCode::findOrFail($id));
-
-        $show->id('Id');
-        $show->name('Name');
-        $show->code('Code');
-        $show->type('Type');
-        $show->value('Value');
-        $show->total('Total');
-        $show->used('Used');
-        $show->min_amount('Min amount');
-        $show->not_before('Not before');
-        $show->not_after('Not after');
-        $show->enabled('Enabled');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new CouponCode);
-
-        $form->text('name', 'Name');
-        $form->text('code', 'Code');
-        $form->text('type', 'Type');
-        $form->decimal('value', 'Value');
-        $form->number('total', 'Total');
-        $form->number('used', 'Used');
-        $form->decimal('min_amount', 'Min amount');
-        $form->datetime('not_before', 'Not before')->default(date('Y-m-d H:i:s'));
-        $form->datetime('not_after', 'Not after')->default(date('Y-m-d H:i:s'));
-        $form->switch('enabled', 'Enabled');
-
-        return $form;
-    }
 }
